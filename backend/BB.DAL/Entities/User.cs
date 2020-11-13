dbaa6ed9aecfefae9758601630fb1c7ba57035de
Vitya <vitalii.kenyiz@ukma.edu.ua>
2020-11-13 02:09:51 +0200

    Change property

--- backend/BB.DAL/Entities/User.cs
+++ backend/BB.DAL/Entities/User.cs
@@ -5,7 +5,7 @@ namespace BB.DAL.Entities
     public class User
     {
         public int UserId { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
 
         public List<Card> Cards { get; set; }
