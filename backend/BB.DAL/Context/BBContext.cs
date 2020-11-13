b2293fb10b9961cbcc7c9b15f8fe6881826f8f56
Vitya <vitalii.kenyiz@ukma.edu.ua>
2020-11-13 02:11:49 +0200

    Supplemented

--- backend/BB.DAL/Context/BBContext.cs
+++ backend/BB.DAL/Context/BBContext.cs
@@ -8,7 +8,13 @@ namespace BB.DAL.Context
     public class BBContext : DbContext
     {
         public DbSet<Card> Cards { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Deposit> Deposits { get; set; }
+        public DbSet<CreditBranch> CreditBranches { get; set; }
+        public DbSet<CheckingBranch> CheckingBranches { get; set; }
+        public DbSet<DepositBranch> DepositBranches { get; set; }
 
+        
         public BBContext(DbContextOptions options) : base(options)
         {
         }
@@ -16,8 +22,6 @@ namespace BB.DAL.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Configure();
-
-            List<int> l = new List<int>();
         }
     }
 }
--- backend/BB.DAL/Context/ModelBuilderExtensions.cs
+++ backend/BB.DAL/Context/ModelBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using BB.DAL.Context.EntityConfigurations;
+using BB.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BB.DAL.Context
@@ -8,6 +9,9 @@ namespace BB.DAL.Context
         public static void Configure(this ModelBuilder model)
         {
             model.ApplyConfigurationsFromAssembly(typeof(CardConfiguration).Assembly);
+            model.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+            model.ApplyConfigurationsFromAssembly(typeof(DepositConfiguration).Assembly);
+            model.ApplyConfigurationsFromAssembly(typeof(CreditBranchConfiguration).Assembly);
         }
     }
 }
