f02a50f9d241a78a067ed0e90108bf411ede80f1
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-11-29 09:34:57 +0200

    added an authorization to operations controller

--- backend/BB.API/Controllers/OperationsController.cs
+++ backend/BB.API/Controllers/OperationsController.cs
@@ -1,12 +1,15 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
 using BB.BLL.Interfaces;
 using BB.Common.Dto.Balance;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BB.API.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    [Authorize]
     public class OperationsController : ControllerBase
     {
         private readonly ICheckingBranchService _branchService;
@@ -16,28 +19,43 @@ namespace BB.API.Controllers
             _branchService = checkingBranchService;
         }
 
-        [HttpGet("{cardId}")]
-        public async Task<BalanceDto> CheckBalance(int cardId)
+        [HttpGet("balance/{cardId}")]
+        public async Task<ActionResult<BalanceDto>> CheckBalance(int cardId)
         {
-            return await _branchService.CheckBalance(cardId);
+            if (User.HasClaim(JwtRegisteredClaimNames.Jti, cardId.ToString()))
+            {
+                return Ok(await _branchService.CheckBalance(cardId));
+            }
+
+            return Forbid();
         }
         
-        [HttpPut]
-        public async Task Withdraw(int cardId, decimal amount)
+        [HttpPost("withdraw")]
+        public async Task<IActionResult> Withdraw(int cardId, decimal amount)
         {
-            await _branchService.Withdraw(cardId, amount);
+            if (User.HasClaim(JwtRegisteredClaimNames.Jti, cardId.ToString()))
+            {
+                await _branchService.Withdraw(cardId, amount);
+            }
+            
+            return Forbid();
         }
 
-        [HttpPut]
+        [HttpPost("topup")]
         public async Task TopUp(int cardId, decimal amount)
         {
             await _branchService.TopUp(cardId, amount);
         }
 
-        [HttpPut]
-        public async Task Transfer(int cardId, string targetCardNum, decimal amount)
+        [HttpPost("transfer")]
+        public async Task<IActionResult> Transfer(int cardId, string targetCardNum, decimal amount)
         {
-            await _branchService.Transfer(cardId, targetCardNum, amount);
+            if (User.HasClaim(JwtRegisteredClaimNames.Jti, cardId.ToString()))
+            {
+                await _branchService.Transfer(cardId, targetCardNum, amount);
+            }
+            
+            return Forbid();
         }
     }
 }
