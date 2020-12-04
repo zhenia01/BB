8ca83672785ed4e7bc758d329a299601c51179c3
Vitya <vitalii.kenyiz@ukma.edu.ua>
2020-12-05 00:35:59 +0200

    added logic

--- backend/BB.BLL/Services/CreditBranchService.cs
+++ backend/BB.BLL/Services/CreditBranchService.cs
@@ -6,18 +6,33 @@ using AutoMapper;
 using BB.BLL.Interfaces;
 using BB.BLL.Services.Abstract;
 using BB.DAL.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace BB.BLL.Services
 {
     public class CreditBranchService : BaseService, ICreditBranchService
     {
+        public static decimal CreditPercent { get; } = 2.5m;
+        public static TimeSpan CreditTimeLapse = new TimeSpan(0, 0, 5);
+
         public CreditBranchService(BBContext context, IMapper mapper) : base(context, mapper)
         {
-            
         }
         
-        public async Task PunishForDebts(CancellationToken stoppingToken)
+        public async Task PunishForDebts(DateTime currDate, CancellationToken stoppingToken)
         {
+            var creditBranches = await Context.CreditBranches
+                .Where(c => c.Balance < c.Available)
+                .ToListAsync(stoppingToken);
+
+            foreach (var card in creditBranches)
+            {
+                var diff = card.Available - card.Balance;
+                card.Debt += decimal.Multiply(Math.Abs(diff), CreditPercent) / 100;
+            }
+
+            await Context.SaveChangesAsync(stoppingToken);
+
             // TODO punishments
             // var debtors = Context.CreditBranches
             //     .Where(cb => cb.Balance < cb.Available && cb.WithdrawTime.HasValue && (cb.WithdrawTime.Value - DateTime.Now).Seconds )
