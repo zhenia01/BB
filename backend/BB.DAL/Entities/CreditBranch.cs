0f93d210cb7686309eeb92b98037e486e1576f09
Vitya <vitalii.kenyiz@ukma.edu.ua>
2020-12-05 00:31:48 +0200

    Remove redundant fields

--- backend/BB.DAL/Entities/CreditBranch.cs
+++ backend/BB.DAL/Entities/CreditBranch.cs
@@ -7,8 +7,6 @@ namespace BB.DAL.Entities
         public int CreditBranchId { get; set; }
         public decimal Available { get; set; }
         public decimal Balance { get; set; }
-        public double Interest { get; set; }
-        public DateTime? WithdrawTime { get; set; }
         public decimal? Debt { get; set; }
         
         public Card Card { get; set; }
--- backend/BB.DAL/Entities/Deposit.cs
+++ backend/BB.DAL/Entities/Deposit.cs
@@ -3,9 +3,9 @@
     public class Deposit
     {
         public int DepositId { get; set; }
-        public int DepSum { get; set; }
+        public decimal DepSum { get; set; }
         public double Percent { get; set; }
-        public string CurrencyOfDeposit { get; set; }
+
         public int Term { get; set; }
         public bool PaymentsToDeposit { get; set; }
         public bool CanBeTerminated { get; set; }
